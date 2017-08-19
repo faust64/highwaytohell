4b6b66ffaad61338be2444e03b4ecb6231633b6c
Samuel Martin Moro <samuel@unetresgrossebite.com>
2017-08-20 00:04:21 +0200

    tests(butters): contacts + healthchecks

--- tests/butters.sh
+++ tests/butters.sh
@@ -42,6 +42,30 @@ if ! ./samples.d/butters -d peerio.com -R healthchecks | grep peerio.com; then
     echo failed listing health checks
     ret=`expr $ret + 1`
 fi
+if ! ./samples.d/butters -d peerio.com -R healthchecks -a add -T https://www.google.com --header www.google.com --match Google --checkname GGL; then
+    echo failed registering health check
+    ret=`expr $ret + 1`
+fi
+if ! ./samples.d/butters -d peerio.com -R healthchecks -a get --checkid e2f2dfb2-7928-11e7-91c1-ed90532c5f11 | grep requireunhealthy; then
+    echo failed fetching health check
+    ret=`expr $ret + 1`
+
+if ! ./samples.d/butters -d peerio.com -R healthchecks -a del --checkid e2f2dfb2-7928-11e7-91c1-ed90532c5f11; then
+    echo failed dropping health check
+    ret=`expr $ret + 1`
+fi
+if ! ./samples.d/butters -d peerio.com -R healthchecks -a get --checkid e2f2dfb2-7928-11e7-91c1-ed90532c5f11 | grep -E '^{}$'; then
+    echo healthcheck table inconsistent - dropped health check still showing
+    ret=`expr $ret + 1`
+fi
+if ! ./samples.d/butters -d peerio.com -R healthhistory -a get --checkid e2f2dfb0-7928-11e7-abc0-01fda1e91471 | grep -E 'when.*[0-9]*.*value.*(true|false)'; then
+    echo failed quering history
+    ret=`expr $ret + 1`
+fi
+if ! ./samples.d/butters -R contacts | grep '"active":"yes"'; then
+    echo failed querying contacts
+    ret=`expr $ret + 1`
+fi
 
 echo ret is $ret
 exit $ret
